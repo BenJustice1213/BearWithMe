cd63169e0a790687e52e6089bcc1d81c6e413613
Hurho the Wendigo <ajbrooksdesign@gmail.com>
2026-04-09 15:45:38 -0400

    Uploading Progress

---
+++ Assets/Scripts/AI Logic/Enemies/Smoker.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Smoker : AIEnemy
+{
+    /*
+    For the love of fuck, don't next the Fire class in the
+    Smoker class logic.
+     */
+    public void StartFire()
+    {
+        // Delay at start to account for animation
+        // activeFires.Add(new Fire());
+        // activeFires[activeFires.Count - 1].SetupFire();
+    }
+}
---
+++ Assets/Scripts/AI Logic/Enemies/Wolf.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Wolf : AIEnemy
+{
+
+    public void FindNextCritter()
+    {
+        // Logic to howl, potentially attracting other enemies or intimidating the player
+    }
+}
